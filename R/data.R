a359321d10a4596c12af5ff34ea75d35ef251c64
Christine <christinecheng1229@gmail.com>
2025-11-04 21:07:00 -0500

    added files ignored by git

---
+++ R/data.R
@@ -0,0 +1,24 @@
+# Purpose: Documentation for package dataset(s).
+# Author: Christine Cheng
+# Date: November 4, 2025
+#' Type 2 Restriction Enzymes Metadata
+#'
+#' Dataset of type 2 restriction enzymes from REABSE version 511 database from Oct 28 2025.
+#'
+#' @source Roberts, R.J., Vincze, T., Posfai, J., Macelis, D. REBASE-a database for DNA restriction and modification: enzymes, genes and genomes. Nucleic Acids Res. 43: D298-D299 (2015).
+#'
+#' @format A matrix with 3 columns:
+#' \describe{
+#'  \item{Name}{Name of restriction enzyme.}
+#'  \item{Prototype}{Prototype of restriction enzyme.}
+#'  \item{RecognitionSeq}{Recognition sequence of restriction enzyme, including cleavage site.}
+#' }
+#' @examples
+#' \dontrun{
+#'  Enzymes
+#'  View(Enzymes)
+#'  head(Enzymes)
+#' }
+"Enzymes"
+
+# [END]
